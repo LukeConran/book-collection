df910fafe13685fdba3b0f50b81c6c3be3d4fde7
LukeConran <stlsoccerdude@gmail.com>
2025-09-09 15:10:06 -0500

    Added test for undeveloped attributes

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -10,4 +10,19 @@ RSpec.describe Book, type: :model do
     book = Book.new(title: nil)
     expect(book).not_to be_valid
   end
+
+  it "is not valid without an author" do
+    book = Book.new(author: nil)
+    expect(book).not_to be_valid
+  end
+
+  it "is not valid without a price" do
+    book = Book.new(price: nil)
+    expect(book).not_to be_valid
+  end
+
+  it "is not valid without a published_date" do
+    book = Book.new(published_date: nil)
+    expect(book).not_to be_valid
+  end
 end
--- spec/system/books_spec.rb
+++ spec/system/books_spec.rb
@@ -5,22 +5,61 @@ RSpec.describe "Books", type: :system do
     driven_by(:rack_test)
   end
 
-  it "creates a book when title is present" do
+  it "creates a book when all attributes are present" do
     visit new_book_path
 
     fill_in "Title", with: "The Hobbit"
+    fill_in "Author", with: "Tolkien"
+    fill_in "Price", with: "12.99"
+    fill_in "Published date", with: Date.today
+
     click_button "Create Book"
 
     expect(page).to have_content("Book was successfully created")
     expect(Book.last.title).to eq("The Hobbit")
+    expect(Book.last.author).to eq("Tolkien")
+    expect(Book.last.price).to eq(12.99)
+    expect(Book.last.published_date).to eq(Date.today)
   end
 
   it "shows an error when title is blank" do
     visit new_book_path
-
     fill_in "Title", with: ""
+    fill_in "Author", with: "Tolkien"
+    fill_in "Price", with: "12.99"
+    fill_in "Published date", with: Date.today
     click_button "Create Book"
-
     expect(page).to have_content("Title can't be blank")
   end
+
+  it "shows an error when author is blank" do
+    visit new_book_path
+    fill_in "Title", with: "The Hobbit"
+    fill_in "Author", with: ""
+    fill_in "Price", with: "12.99"
+    fill_in "Published date", with: Date.today
+    click_button "Create Book"
+    expect(page).to have_content("Author can't be blank")
+  end
+
+  it "shows an error when price is blank" do
+    visit new_book_path
+    fill_in "Title", with: "The Hobbit"
+    fill_in "Author", with: "Tolkien"
+    fill_in "Price", with: ""
+    fill_in "Published date", with: Date.today
+    click_button "Create Book"
+    expect(page).to have_content("Price can't be blank")
+  end
+
+  it "shows an error when published date is blank" do
+    visit new_book_path
+    fill_in "Title", with: "The Hobbit"
+    fill_in "Author", with: "Tolkien"
+    fill_in "Price", with: "12.99"
+    fill_in "Published date", with: ""
+    click_button "Create Book"
+    expect(page).to have_content("Published date can't be blank")
+  end
 end
+
